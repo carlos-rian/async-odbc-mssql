5a4cb4e501518399e2472067b0bbcc4ef06d3852
carlos.rian <carlos.rian@quartile.com>
2022-04-19 16:54:54 -0300

    add new async func

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,23 +1,39 @@
+#[warn(unused_variables)]
+#[warn(unused_imports)]
+#[warn(dead_code)]
+
+use tiberius::error::Error;
 use tiberius::{Client, Config};
 use tokio::net::TcpStream;
-use tokio_util::compat::{TokioAsyncWriteCompatExt};
+use tokio_util::compat::TokioAsyncWriteCompatExt;
+//use pyo3::{prelude::*, wrap_pyfunction};
 
-
-#[warn(unused_variables)]
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let conn: String = "server=tcp:localhost,1433;username=SA;password=Super&-2;TrustServerCertificate=true".to_string();
-    
-    let config: Config = Config::from_ado_string(conn.as_str())?;
+async fn query(q: &str, conn_str: &str) -> Result<Vec<Vec<tiberius::Row>>, Error> {
+    let config: Config = Config::from_ado_string(conn_str)?;
     let tcp: TcpStream = TcpStream::connect(config.get_addr()).await?;
     tcp.set_nodelay(true)?;
 
     let mut conn = Client::connect(config, tcp.compat_write()).await?; 
 
-    let res = conn.query("SELECT @P1", &[&-4i32]).await?;
-    let _result = res.into_results().await?;
-
-    print!("{:?}", _result);
+    let query_stream = conn.query(q, &[]).await?;
+    let rows = query_stream.into_results().await?;
     
+    Ok(rows)
+}
+
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let conn_str = "\
+        server=tcp:localhost,1433; \
+        username=SA; \
+        password=Super&-23; \
+        database=master; \
+        TrustServerCertificate=true";
+    let q = "select * from spt_monitor;";
+
+    let result = query(&q, &conn_str).await?;
+    println!("{:?}", result);
+
     Ok(())
 }
