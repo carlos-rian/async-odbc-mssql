a69cdf3061c50c88f737f31fba8393b04e5497d0
carlos.rian <carlos.rian@quartile.com>
2022-09-23 15:34:59 -0300

    delete 30 files

--- core/bkp/postgres/tables_query.sql
+++
@@ -1,5 +0,0 @@
-SELECT tbl.relname AS table_name
-FROM pg_class AS tbl
-INNER JOIN pg_namespace AS namespace ON namespace.oid = tbl.relnamespace
-WHERE tbl.relkind = 'r' AND namespace.nspname = $1
-ORDER BY table_name;
