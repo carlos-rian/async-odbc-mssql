3bade2196dabda2db631e8db7ae79ea40ce0c5ad
carlos.rian <carlos.rian@quartile.com>
2022-09-22 22:15:53 -0300

    update 3 files and delete 2 files

---
+++ core/src/column.rs
@@ -0,0 +1,108 @@
+use crate::model_extensions::ScalarFieldExt;
+use itertools::Itertools;
+use prisma_models::{Field, ModelProjection, RelationField, ScalarField};
+use quaint::ast::{Column, Row};
+use std::convert::AsRef;
+
+pub struct ColumnIterator {
+    inner: Box<dyn Iterator<Item = Column<'static>> + 'static>,
+}
+
+impl Iterator for ColumnIterator {
+    type Item = Column<'static>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+}
+
+impl From<Vec<Column<'static>>> for ColumnIterator {
+    fn from(v: Vec<Column<'static>>) -> Self {
+        Self {
+            inner: Box::new(v.into_iter()),
+        }
+    }
+}
+
+pub trait AsRow {
+    fn as_row(&self) -> Row<'static>;
+}
+
+pub trait AsColumns {
+    fn as_columns(&self) -> ColumnIterator;
+}
+
+impl AsColumns for ModelProjection {
+    fn as_columns(&self) -> ColumnIterator {
+        let cols: Vec<Column<'static>> = self
+            .fields()
+            .flat_map(|f| f.as_columns())
+            .unique_by(|c| c.name.clone())
+            .collect();
+
+        ColumnIterator::from(cols)
+    }
+}
+
+impl AsRow for ModelProjection {
+    fn as_row(&self) -> Row<'static> {
+        let cols: Vec<Column<'static>> = self.as_columns().collect();
+        Row::from(cols)
+    }
+}
+
+pub trait AsColumn {
+    fn as_column(&self) -> Column<'static>;
+}
+
+impl AsColumns for Field {
+    fn as_columns(&self) -> ColumnIterator {
+        match self {
+            Field::Scalar(ref sf) => ColumnIterator::from(vec![sf.as_column()]),
+            Field::Relation(ref rf) => rf.as_columns(),
+            Field::Composite(_) => unimplemented!(),
+        }
+    }
+}
+
+impl AsColumns for RelationField {
+    fn as_columns(&self) -> ColumnIterator {
+        self.scalar_fields().as_columns()
+    }
+}
+
+impl<T> AsColumns for &[T]
+where
+    T: AsColumn,
+{
+    fn as_columns(&self) -> ColumnIterator {
+        let inner: Vec<_> = self.iter().map(|x| x.as_column()).collect();
+        ColumnIterator::from(inner)
+    }
+}
+
+impl<T> AsColumns for Vec<T>
+where
+    T: AsColumn,
+{
+    fn as_columns(&self) -> ColumnIterator {
+        let inner: Vec<_> = self.iter().map(|x| x.as_column()).collect();
+        ColumnIterator::from(inner)
+    }
+}
+
+impl<T> AsColumn for T
+where
+    T: AsRef<ScalarField>,
+{
+    fn as_column(&self) -> Column<'static> {
+        let sf = self.as_ref();
+
+        // Unwrap is safe: SQL connectors do not anything other than models as field containers.
+        let full_table_name = sf.container.as_model().unwrap().db_name_with_schema();
+        let col = sf.db_name().to_string();
+
+        let column = Column::from((full_table_name, col)).type_family(sf.type_family());
+        column.default(quaint::ast::DefaultValue::Generated)
+    }
+}
--- core/src/main.rs
+++ core/src/main.rs
@@ -1,31 +1,23 @@
+use quaint::error::Error;
 use quaint::{prelude::*, single::Quaint};
-use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
-pub struct Procedure {
-    /// Procedure name.
-    pub name: String,
-    /// The definition of the procedure.
-    pub definition: Option<String>,
-}
-
-
-#[tokio::main]
-async fn main() -> Result<(), quaint::error::Error> {
+async fn sql_test() -> Result<ResultSet, Error> {
     let conn = Quaint::new("file:///tmp/example.db").await?;
     let sql = "select 1 as number;";
-    let rows = conn.query_raw(sql, &[]).await?;
-
-    let mut procedures = Vec::with_capacity(rows.len());
-
-    for row in rows.into_iter() {
-        procedures.push(Procedure {
-            name: row.get_expect_string("name"),
-            definition: row.get_string("definition"),
-        });
-    }
-
-    println!("{:#?}", data);
-
+    let result = conn.query_raw(sql, &[]).await?;
+    Ok(result)
+}
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let rows =  sql_test().await?;
+    println!("{:#?}", rows);
     Ok(())
 }
+
+//#[pyfunction]
+//fn call_sql_test(py: Python<'_>) -> PyResult<&PyAny>{
+//    pyo3_asyncio::async_std::future_into_py(py, async move {
+//        sql_test().await;
+//        Ok(Python::with_gil(|py| py.None()))
+//    })
+//}
